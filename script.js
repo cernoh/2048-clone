a4e434878ee3391055f44c555ee7e20b22fca158
cernoh <34456152+cernoh@users.noreply.github.com>
2022-03-12 13:16:12 +0000

    Added Tiles

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 import Grid from "./Grid.js"
+import Tile from "./Tile.js"
 
 const gameBoard = document.getElementById("game-board")
 
 const grid = new Grid(gameBoard)
+
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+
+function setupInput() {
+    window.addEventListener("keydown", handleInput, {once: true})
+}
+
+function handleInput(e) {
+    switch(e.key){
+        case "ArrowUp":
+            moveUp()
+        case "ArrowDown":
+            moveDown()
+        case "ArrowLeft":
+            moveLeft()
+        case "ArrowRight":
+            moveRight()
+    }
+}
